0a2d96423d338bc92a9f15559fd30f9f66dd3eb6
Rayl1ght <105597943+Rayl1ght@users.noreply.github.com>
2023-04-20 01:38:47 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import Header from './components/Layout/Header.vue'
+    import Hero from './components/Hero.vue'
+</script>
+
+<template>
+  <div class="container">
+   <Header/>
+   <Hero/>
+   </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.cdnfonts.com/css/clash-display');
+@import url('https://fonts.cdnfonts.com/css/satoshi');
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Clash Display', sans-serif;
+    font-size: 16px;
+//  font-family: 'Satoshi', sans-serif;
+}
+
+img {
+    max-width: 100%;
+}
+
+.container {
+    max-width: 1440px;
+    margin: auto;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="hero">
+        <div class="hero-info">
+            <div class="hero-info-text">
+                <h2 class="hero-info__title">Cool PC</h2>
+                <span class="hero-info__description">900 $</span>
+            </div>
+            <a href="#" class="hero-info-link">BUY</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+        background: url('https://digital-discount.ru/wp-content/uploads/d/9/0/d9053a5373e7fb7b2d47f8aca75db348.jpeg');
+        height: 704px;
+        background: cover;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 80px;
+
+        &-img {
+            max-width: 100%;
+        }
+
+        &-info {
+            background: #fff;
+            max-width: 630px;
+            padding: 50px 55px;
+            height: 444px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            &__title {
+                font-family: 'Satoshi', sans-serif;
+                font-weight: 400;
+                line-height: 1.5;
+                margin: 0 0 20px;
+            }
+
+            &__description {
+                color: #5b5676;
+                font-size: 18px;
+            }
+
+            &-link {
+                color: #2A254B;
+                text-decoration: none;
+
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }  
+</style>
