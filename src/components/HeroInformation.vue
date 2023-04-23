e07b7e961411b84a5bd6cd91a2fcb7ca2d7e78e6
Rayl1ght <105597943+Rayl1ght@users.noreply.github.com>
2023-04-24 00:40:21 +0300

    4

--- src/components/HeroInformation.vue
+++ src/components/HeroInformation.vue
@@ -1,33 +1,30 @@
 <template>
-    <div class="hero">
-        <div class="hero-info">
-            <HeroInformation
-                title="Cool PC"
-                description="900 $"
-                link="#"/>
-            
-        </div>
+    <div class="hero-info-text">
+        <h2 class="hero-info__title">{{title}}</h2>
+        <span class="hero-info__description">{{description}}</span>
     </div>
+    <a :href="link" class="hero-info-link">BUY</a>
 </template>
 
 <script setup>
-    import HeroInformation from './HeroInformation.vue'
+    const props = defineProps({
+        title: {
+            type: String,
+            required: false
+        },
+        description: {
+            type: String,
+            required: false
+        },
+        link: {
+            type: String,
+            required: false
+        }
+    })
 </script>
 
 <style lang="scss" scoped>
     .hero {
-        background: url('https://digital-discount.ru/wp-content/uploads/d/9/0/d9053a5373e7fb7b2d47f8aca75db348.jpeg');
-        height: 704px;
-        background: cover;
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        padding-right: 80px;
-
-        &-img {
-            max-width: 100%;
-        }
-
         &-info {
             background: #fff;
             max-width: 630px;
@@ -58,5 +55,5 @@
                 }
             }
         }
-    }  
+    }
 </style>
